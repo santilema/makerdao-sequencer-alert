fb959c1e18e291b1350d687252a00ba685ab13e8
santi <lema.sx@gmail.com>
2024-03-21 15:05:27 +0100

    added compiled index to git

---
+++ function/index.js
@@ -0,0 +1,82 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const etherscan_1 = require("./utils/etherscan");
+const alerts_1 = require("./utils/alerts");
+const sequencerABI_json_1 = __importDefault(require("../sequencerABI.json"));
+const ethers_1 = require("ethers");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const discordWebhook = process.env.DISCORD_WEBHOOK;
+const rpcURL = process.env.RPC_URL;
+const sequencerAddress = process.env.SEQUENCER_ADDRESS;
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+const blocksWindow = Number(process.env.BLOCKS_WINDOW);
+/**
+ * Monitors job contract activities within a specified number of recent blocks and
+ * sends a Discord alert if no job was worked on.
+ *
+ * This handler function connects to a blockchain via an Ethereum JSON RPC provider
+ * and interacts with a sequencer contract to enumerate job contracts. It checks each
+ * job contract to see if the 'Work' event was emitted in the last `blocksWindow` blocks,
+ * indicating that work has been done. If no 'Work' event is found for any job within
+ * the specified block window, it sends an alert to a Discord channel. Otherwise, it
+ * logs that work has been done.
+ *
+ * @param event Optional parameter that can be used to pass event data to the handler,
+ *              not used in the current implementation.
+ * @param context Optional parameter that can be used to provide runtime information to
+ *                the handler, not used in the current implementation.
+ */
+const handler = async (event = null, context = null) => {
+    try {
+        let alert = true;
+        // Connection to sequencer contract
+        const provider = new ethers_1.ethers.JsonRpcProvider(rpcURL);
+        const sequencerContract = new ethers_1.ethers.Contract(sequencerAddress, sequencerABI_json_1.default, provider);
+        // Define window of blocks
+        const latestBlockNumber = await provider.getBlockNumber(); //current block
+        const earliestBlockNumber = latestBlockNumber - blocksWindow;
+        console.log("latestBlockNumber", latestBlockNumber, "earliestBlockNumber", earliestBlockNumber);
+        // Get the jobs
+        const numJobs = await sequencerContract.numJobs();
+        console.log("numJobs", numJobs);
+        // fetch Work events in parallel
+        const jobPromises = Array.from({ length: Number(numJobs) }, async (_, i) => {
+            const jobAddress = await sequencerContract.jobAt(i);
+            // Get Work event signature for each job
+            const workEventSignatureHash = await (0, etherscan_1.getWorkEventSignature)(jobAddress, etherscanApiKey);
+            // Filter looking for event signature meaning that work was done
+            const filter = {
+                address: jobAddress,
+                fromBlock: earliestBlockNumber,
+                toBlock: latestBlockNumber,
+                topics: [
+                    workEventSignatureHash
+                        ? workEventSignatureHash
+                        : "Work(bytes32,address)",
+                ],
+            };
+            const logs = await provider.getLogs(filter);
+            console.log("logs", jobAddress, logs);
+            return { jobAddress, workEventSignatureHash, logs };
+        });
+        const jobs = await Promise.all(jobPromises);
+        // If no logs were found, then no jobs have been worked on
+        alert = jobs.every((job) => job.logs.length == 0);
+        if (alert) {
+            console.log("No work was done");
+            (0, alerts_1.sendAlert)(discordWebhook, `No job was worked on in the last ${blocksWindow} blocks at block ${latestBlockNumber}`);
+        }
+        else {
+            console.log("Work was done");
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
+exports.handler = handler;
---
+++ function/utils/etherscan.js
@@ -0,0 +1,87 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getWorkEventSignature = exports.fetchContractABI = void 0;
+const path_1 = __importDefault(require("path"));
+const promises_1 = require("fs/promises");
+const ethers_1 = require("ethers");
+/**
+ * Fetches the ABI for a given contract from Etherscan.
+ *
+ * This function sends a GET request to the Etherscan API to retrieve the ABI
+ * of a contract specified by its address. The Etherscan API key is required
+ * to authenticate the request. If successful, it parses the ABI from the response
+ * and returns it. Otherwise, it logs an error and returns null.
+ *
+ * @param address The Ethereum address of the contract for which to fetch the ABI.
+ * @param etherscanApiKey Your Etherscan API key for authenticating the request.
+ * @returns The ABI of the specified contract as a JSON object, or null if the
+ *          fetch operation fails or if the ABI is not available.
+ */
+const fetchContractABI = async (address, etherscanApiKey) => {
+    const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${etherscanApiKey}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.status === "1" && data.message === "OK") {
+            return JSON.parse(data.result);
+        }
+        else {
+            console.error("Error fetching contract ABI");
+            return null;
+        }
+    }
+    catch (error) {
+        console.error("Error fetching ABI");
+        return null;
+    }
+};
+exports.fetchContractABI = fetchContractABI;
+/**
+ * Retrieves the signature hash of the 'Work' event for a given job contract.
+ *
+ * First, it attempts to read the contract's ABI from a local JSON file. If the file
+ * does not exist or reading fails, it fetches the ABI from Etherscan using the provided
+ * Etherscan API key and saves the ABI locally. It then searches the ABI for the 'Work'
+ * event and constructs its signature hash. If the 'Work' event is found, the function
+ * returns its signature hash. Otherwise, it logs an error and returns undefined.
+ *
+ * @param jobAddress The Ethereum address of the job contract.
+ * @param etherscanApiKey Your Etherscan API key for fetching the ABI, if necessary.
+ * @returns The signature hash of the 'Work' event if found, or undefined if the
+ *          'Work' event is not found in the ABI or if fetching the ABI fails.
+ */
+const getWorkEventSignature = async (jobAddress, etherscanApiKey) => {
+    let abi;
+    const abiPath = path_1.default.join(__dirname, "../..", "jobsABIs", `${jobAddress}.json`);
+    try {
+        const abiJson = await (0, promises_1.readFile)(abiPath, "utf8");
+        abi = JSON.parse(abiJson);
+    }
+    catch (error) {
+        console.error(`Failed to read ABI for ${jobAddress}:`, error);
+        abi = await (0, exports.fetchContractABI)(jobAddress, etherscanApiKey);
+        if (abi) {
+            await (0, promises_1.writeFile)(abiPath, JSON.stringify(abi), "utf-8");
+            console.log(`ABI for ${jobAddress} saved to ${abiPath}`);
+        }
+        else {
+            console.error(`Failed to fetch ABI for ${jobAddress}`);
+            return undefined;
+        }
+    }
+    for (const item of abi) {
+        if (item.type == "event" && item.name == "Work") {
+            const eventName = item.name;
+            const paramsTypes = item.inputs.map((input) => input.type).join(",");
+            const eventSignature = `${eventName}(${paramsTypes})`;
+            console.log(eventSignature);
+            return ethers_1.ethers.id(eventSignature);
+        }
+    }
+    console.error(`Work event not found in ABI for  ${jobAddress}`);
+    return undefined;
+};
+exports.getWorkEventSignature = getWorkEventSignature;
